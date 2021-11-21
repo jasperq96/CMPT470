e84ebc2c98fbe6687e361c31e5140b33695ed911
Matt Fung <chf2@sfu.ca>
2021-11-20 23:21:46 -0800

    3: Implement multer file saving

---
+++ dello-box-server/src/controllers/fileController.ts
@@ -0,0 +1,52 @@
+import logging from '../config/logging';
+import { Request, Response, NextFunction } from 'express';
+import { Knex } from '../config/postgres';
+import { fileNegativeOrNanInputError, fileDNEError, fileMimetypeError } from 'util/errorMessages';
+import { isInvalidInput } from 'util/isInvalidInput';
+
+const NAMESPACE = 'File Control';
+const TABLE_NAME = 'file';
+
+const inputtedReqFile = (req: any, filepath: any, mimetype: any) => {
+  const { filename, size } = req.file;
+  return { filename: filename, filepath: filepath, mimetype: mimetype, size: size };
+};
+
+const getFileById = async (req: Request, res: Response, next: NextFunction) => {
+  logging.info(NAMESPACE, `GETTING AN ${TABLE_NAME.toUpperCase()} BY ID`);
+  const fileId: number = +req.params.fileId;
+  if (isInvalidInput(fileId)) {
+    res.status(400).send(fileNegativeOrNanInputError);
+    return;
+  }
+
+  try {
+    const retrievedFilePath = await Knex.select('filepath').from(TABLE_NAME).where('id', '=', fileId);
+    logging.info(NAMESPACE, `RETRIEVED ${TABLE_NAME.toUpperCase()} ${fileId}`, retrievedFilePath);
+    if (!retrievedFilePath.length) {
+      res.status(404).send(fileDNEError);
+      return;
+    }
+    res.status(200).sendFile(`/home/node/app/${retrievedFilePath[0].filepath}`);
+  } catch (error: any) {
+    logging.error(NAMESPACE, error.message, error);
+    res.status(500).send(error);
+  }
+};
+
+const addFile = async (req: any, res: Response, next: NextFunction) => {
+  logging.info(NAMESPACE, `CREATING AN ${TABLE_NAME.toUpperCase()}`);
+  try {
+    const filepath = req.file.path;
+    const mimetype = req.file.mimetype;
+    const createdFile = await Knex.insert(inputtedReqFile(req, filepath, mimetype)).into(TABLE_NAME);
+    const retrievedCreatedFile = await Knex.select('*').from(TABLE_NAME).whereIn('id', createdFile);
+    logging.info(NAMESPACE, `CREATED ${TABLE_NAME.toUpperCase()}`, retrievedCreatedFile);
+    res.status(201).send(retrievedCreatedFile);
+    // res.sendStatus(204);
+  } catch (error: any) {
+    res.status(400).send(fileMimetypeError);
+  }
+};
+
+export default { getFileById, addFile };
--- dello-box-server/src/controllers/userInfoController.ts
+++ dello-box-server/src/controllers/userInfoController.ts
@@ -5,16 +5,16 @@ import { Knex } from '../config/postgres';
 const NAMESPACE = 'User Control';
 const TABLE_NAME = 'user_info';
 
-export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+export const getUserInfo = async (req: Request, res: Response, next: NextFunction) => {
   logging.info(NAMESPACE, `GETTING LIST OF ${TABLE_NAME}S`);
   try {
-    const retrievedDummies = await Knex.select('*').from(TABLE_NAME);
-    logging.info(NAMESPACE, `RETRIEVED ${TABLE_NAME}S:`, retrievedDummies);
-    res.status(200).send(retrievedDummies);
+    const retrievedUserInfo = await Knex.select('*').from(TABLE_NAME);
+    logging.info(NAMESPACE, `RETRIEVED ${TABLE_NAME}S:`, retrievedUserInfo);
+    res.status(200).send(retrievedUserInfo);
   } catch (error: any) {
     logging.error(NAMESPACE, error.message, error);
     res.status(500).send(error.message);
   }
 };
 
-export default { getUsers };
+export default { getUserInfo };
