b31615ffc04c42807a944ef59f41aab84f3db514
Matt Fung <chf2@sfu.ca>
2021-11-30 19:33:56 -0800

    12: Delete Dummy API, update postman collection

--- dello-box-server/src/db/models/dummyModel.ts
+++
@@ -1,5 +0,0 @@
-export interface Dummy {
-  id: number;
-  name: string;
-  description: string;
-}
--- dello-box-server/src/routes/dummyRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/dummyController';
-
-const router = express.Router();
-
-router.get('', controller.getDummies);
-
-export = router;
