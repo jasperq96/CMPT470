1a453aca553f4711131431af07aa59fd2420bdaf
ygyang <ygyang@sfu.ca>
2021-12-01 15:49:12 -0800

    Added email,phone,name fields to signup

--- dello-box-client/src/Pages/Signup.js
+++ dello-box-client/src/Pages/Signup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Navbar,Container,Form,Button, Col} from 'react-bootstrap'
+import {Navbar,Container,Form,Button, Col, Row} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 export default function Signup() {
     return (
@@ -17,11 +17,28 @@ export default function Signup() {
             <Container>
                 <Col xs>
                     <Form>
-                        <Form.Group className="mb-3 mt-5" ontrolId="formBasicEmail">
+                        <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
                             <Form.Label>User Name</Form.Label>
                             <Form.Control type="Username" placeholder="Enter Username" />
                         </Form.Group>
-
+                        <Row>
+                        <Form.Group as={Col} md="6" className="position-relative mb-3" controlId="formBasicFirstName">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="FirstName" placeholder="Enter First Name" />
+                        </Form.Group>
+                        <Form.Group as={Col} md="6" className="position-relative mb-3" controlId="formBasicLastName">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type="LastName" placeholder="Enter Last Name"/>
+                        </Form.Group>
+                        </Row>
+                        <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="PhoneNumber" placeholder="Enter Phone Number"/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="Email" placeholder="Enter Email"/>
+                        </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password" />
