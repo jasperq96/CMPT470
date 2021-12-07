e4425d7080039b73830cf348aecc10b7c778aea0
ygyang <ygyang@sfu.ca>
2021-12-07 15:30:49 -0800

    Finished calendar

--- dello-box-client/src/pages/Calendar.js
+++ dello-box-client/src/pages/Calendar.js
@@ -1,9 +1,48 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Calendarlib from 'react-calendar';
+import DatePicker from 'react-datepicker';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'react-calendar/dist/Calendar.css';
+import 'react-datepicker/dist/react-datepicker.css';
 export default function Calendar() {
+  const [date, setDate] = useState(new Date());
+  const onDate = (newDate) => {
+    if (newDate !== null) {
+      setDate(newDate);
+    }
+  };
+  const temp_dates = [
+    {
+      date: '2021-11-14T10:30:00.000Z', //this is nov 14
+      info: 'this is what your doing on nov 11'
+    },
+    {
+      date: '2021-11-13T10:30:00.000Z',
+      info: 'this is to check if i can put multiple things on one list' //this is
+    },
+    {
+      date: '2021-11-13T10:30:00.000Z',
+      info: 'this is to check if i can test put multiple things on one list' //this is
+    }
+  ];
   return (
     <div>
-      <h1>Placeholder for Calendar</h1>
+      <Container fluid>
+        <Row>
+          <Col lg>
+            <Calendarlib onChange={onDate} value={date} />
+            <DatePicker selected={date} onChange={(date) => setDate(date)} readOnly />
+          </Col>
+          <Col lg>
+            {temp_dates
+              .filter((stored_date) => stored_date.date.substring(0, 10) === date.toISOString().substring(0, 10))
+              .map((filtered_Dates) => (
+                <li>{filtered_Dates.info}</li>
+              ))}
+            {console.log(date.toISOString)}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
