90182bc5af476d6f4ab1dffaa679f5bf5e8fa058
jasperq <jasperq@sfu.ca>
2021-12-07 17:52:31 -0800

    Edit API for contact nicknames created

--- dello-box-server/src/controllers/contactsController.ts
+++ dello-box-server/src/controllers/contactsController.ts
@@ -4,10 +4,13 @@ import { Knex } from 'config/postgres';
 import { isInvalidInput } from 'utils/isInvalidInput';
 import { contactNegativeOrNanInputError, contactDNEError } from 'utils/errorMessages';
 import { getItems } from './requestTemplates/getAllRequest';
-import { editItemById } from './requestTemplates/editByIdRequest';
 
 const NAMESPACE = 'Contact List Control';
 const TABLE_NAME = 'contact_list';
+
+// const inputtedReqBody = (req: Request) => {
+//   return { user_id: userId, start_date: startDate, end_date: endDate, title: title, notes: notes };
+// };
 
 const getContacts = async (req: Request, res: Response, next: NextFunction) => {
   await getItems(req, res, next, NAMESPACE, TABLE_NAME);
@@ -54,9 +57,41 @@ const getContactsByUserId = async (req: Request, res: Response, next: NextFuncti
   }
 };
 
-// const editUserInfoByUserId = async (req: Request, res: Response, next: NextFunction) => {
-//   const userId: number = +req.params.userId;
-//   await editItemById(req, res, next, NAMESPACE, TABLE_NAME, userInfoNegativeOrNanInputError, userInfoDNEError, inputtedReqBody(req), userId, 'user_id');
-// };
+const editUserInfoByUserId = async (req: Request, res: Response, next: NextFunction) => {
+  const userId: number = +req.params.userId;
+  const contactId: number = req.body.contactId;
+  const newNickname: string = req.body.newNickname;
 
-export default { getContacts, getContactsByUserId };
+  logging.info(NAMESPACE, `EDITING A ${TABLE_NAME.toUpperCase()} BY ID`);
+  if (isInvalidInput(userId)) {
+    res.status(400).send(contactNegativeOrNanInputError);
+    return;
+  }
+
+  try {
+    const listOfNicknames = await Knex.select('user_info.contact_nicknames').from('user_info').where('user_id', userId).first();
+    listOfNicknames['contact_nicknames'] = JSON.parse(listOfNicknames['contact_nicknames']);
+    listOfNicknames['contact_nicknames'][String(contactId)] = newNickname;
+    var new_list = [JSON.stringify(listOfNicknames['contact_nicknames'])];
+
+    for (let i = 0; i < listOfNicknames['contact_nicknames'].length; i++) {
+      if (new_list[i] == '"') {
+        new_list[i] = '\\"';
+      }
+    }
+
+    const editByItemId = await Knex.update({ contact_nicknames: new_list }).into('user_info').where('user_info.user_id', '=', userId);
+    if (!editByItemId) {
+      res.status(404).send(contactDNEError);
+      return;
+    }
+    const retrievedEditedItem = await Knex.select('contact_nicknames').from('user_info').where('user_info.user_id', '=', userId);
+    logging.info(NAMESPACE, `EDITED USER_INFO WITH ID ${userId}`, retrievedEditedItem);
+    res.status(201).send(retrievedEditedItem);
+  } catch (error: any) {
+    logging.error(NAMESPACE, error.message, error);
+    res.status(500).send(error);
+  }
+};
+
+export default { getContacts, getContactsByUserId, editUserInfoByUserId };
--- dello-box-server/src/routes/contactsRoute.ts
+++ dello-box-server/src/routes/contactsRoute.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('', controller.getContacts);
 router.get('/:userId', controller.getContactsByUserId);
+router.put('/:userId', controller.editUserInfoByUserId);
 
 export = router;
