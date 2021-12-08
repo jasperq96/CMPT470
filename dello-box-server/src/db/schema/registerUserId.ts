ac1efb07b7cf1d3a1a1dbe18c8723ea0517d01ed
jasperq <jasperq@sfu.ca>
2021-12-08 14:41:57 -0800

    Added middleware for userId validation for ADD and DELETE

---
+++ dello-box-server/src/db/schema/registerUserId.ts
@@ -0,0 +1,9 @@
+import { check } from 'express-validator';
+
+const userIdSchema = [
+  check('contactId')
+    .custom((val) => /^([1-9])\d*$/.test(val))
+    .withMessage('Has to be an integer that does not start with a 0')
+];
+
+export { userIdSchema as registerUserId };
--- dello-box-server/src/routes/contactsRoute.ts
+++ dello-box-server/src/routes/contactsRoute.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import controller from '../controllers/contactsController';
 import { registerNickName } from 'db/schema/registerNickname';
+import { registerUserId } from 'db/schema/registerUserId';
 import { validateNickname } from 'middlewares/validateNickname.mw';
+import { validateUserId } from 'middlewares/validateUserId.mw';
 
 const router = express.Router();
 
 router.get('', controller.getContacts);
 router.get('/:userId', controller.getContactsByUserId);
 router.put('/:userId', registerNickName, validateNickname, controller.editNicknameOfContacts);
-router.delete('/:userId', controller.deleteContactById);
-router.post('/:userId', controller.addContactById);
+router.delete('/:userId', registerUserId, validateUserId, controller.deleteContactById);
+router.post('/:userId', registerUserId, validateUserId, controller.addContactById);
 
 export = router;
