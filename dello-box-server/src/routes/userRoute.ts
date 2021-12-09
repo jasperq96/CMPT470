013e54eaafbfdfd9746d359653b804b060e72e9e
Matt Fung <chf2@sfu.ca>
2021-12-08 23:08:55 -0800

    28: Add middleware for POST a user

--- dello-box-server/src/routes/userRoute.ts
+++ dello-box-server/src/routes/userRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import controller from '../controllers/userController';
+import { registerUser } from 'db/schema/registerUser';
+import { validateUser } from 'middlewares/validateUser.mw';
 
 const router = express.Router();
 
 router.get('', controller.getUsers);
-router.post('', controller.createUser);
+router.post('', registerUser, validateUser, controller.createUser);
 
 export = router;
