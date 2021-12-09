29b3f3b7e80d82eaf396e2a9bb35d8d6af3f294d
jasperq <jasperq@sfu.ca>
2021-12-09 13:47:35 -0800

    Updated registerUsername for escape characters and min length

--- dello-box-server/src/db/schema/registerUsername.ts
+++ dello-box-server/src/db/schema/registerUsername.ts
@@ -1,9 +1,5 @@
 import { check } from 'express-validator';
 
-const usernameSchema = [
-  check('username')
-    .custom((val) => /^[^<>]+$/.test(val))
-    .withMessage('Cannot contain < or >')
-];
+const usernameSchema = [check('username').escape().isLength({ min: 4 }).withMessage('Must be a minimum of 4 characters')];
 
 export { usernameSchema as registerUsername };
