a06780ed285cb26e30dae2ad673e4f942f463493
ygyang <ygyang@sfu.ca>
2021-12-09 16:46:05 -0800

    finished moving between columns and within columns

--- dello-box-client/src/pages/TaskPages/Tasks.js
+++ dello-box-client/src/pages/TaskPages/Tasks.js
@@ -7,6 +7,7 @@ const tasksfrombackend = [
     id: 3,
     user_id: 1,
     col_id: 1,
+    index: 0,
     start_date: '2021-11-14T10:30:00.000Z',
     end_date: '2021-11-18T16:30:00.000Z',
     title: 'task1',
@@ -14,8 +15,9 @@ const tasksfrombackend = [
   },
   {
     id: 2,
-    user_id: 2,
-    col_id: 0,
+    user_id: 1,
+    col_id: 2,
+    index: 0,
     start_date: '2021-11-16T09:00:00.000Z',
     end_date: '2021-11-24T18:30:00.000Z',
     title: 'task2',
@@ -25,6 +27,7 @@ const tasksfrombackend = [
     id: 5,
     user_id: 1,
     col_id: 1,
+    index: 1,
     start_date: '2021-11-13T10:30:00.000Z',
     end_date: '2021-11-19T16:30:00.000Z',
     title: 'task3',
@@ -32,51 +35,193 @@ const tasksfrombackend = [
   },
   {
     id: 4,
-    user_id: 2,
-    col_id: 0,
+    user_id: 1,
+    col_id: 2,
+    index: 1,
     start_date: '2021-11-17T09:00:00.000Z',
     end_date: '2021-11-25T18:30:00.000Z',
     title: 'task4',
     notes: 'Blah blah blah'
+  },
+  {
+    id: 6,
+    user_id: 1,
+    col_id: 1,
+    index: 2,
+    start_date: '2021-11-13T10:30:00.000Z',
+    end_date: '2021-11-19T16:30:00.000Z',
+    title: 'task3',
+    notes: 'More notes here'
+  },
+  {
+    id: 7,
+    user_id: 1,
+    col_id: 1,
+    index: 3,
+    start_date: '2021-11-13T10:30:00.000Z',
+    end_date: '2021-11-19T16:30:00.000Z',
+    title: 'task3',
+    notes: 'More notes here'
+  },
+  {
+    id: 8,
+    user_id: 1,
+    col_id: 1,
+    index: 4,
+    start_date: '2021-11-13T10:30:00.000Z',
+    end_date: '2021-11-19T16:30:00.000Z',
+    title: 'task3',
+    notes: 'More notes here'
+  },
+  {
+    id: 9,
+    user_id: 1,
+    col_id: 1,
+    index: 5,
+    start_date: '2021-11-13T10:30:00.000Z',
+    end_date: '2021-11-19T16:30:00.000Z',
+    title: 'task3',
+    notes: 'More notes here'
+  },
+  {
+    id: 10,
+    user_id: 1,
+    col_id: 1,
+    index: 6,
+    start_date: '2021-11-13T10:30:00.000Z',
+    end_date: '2021-11-19T16:30:00.000Z',
+    title: 'task3',
+    notes: 'More notes here'
   }
 ];
 const columnsfrombackend = [
   {
-    id: 0,
+    id: 1,
     label: 'first column',
     col_order: 0
   },
   {
-    id: 1,
+    id: 2,
     label: 'second column',
     col_order: 1
+  },
+  {
+    id: 3,
+    label: 'third column',
+    col_order: 2
+  },
+  {
+    id: 4,
+    label: 'third column',
+    col_order: 3
+  },
+  {
+    id: 5,
+    label: 'third column',
+    col_order: 4
+  },
+  {
+    id: 6,
+    label: 'third column',
+    col_order: 5
+  },
+  {
+    id: 7,
+    label: 'third column',
+    col_order: 6
+  },
+  {
+    id: 8,
+    label: 'third column',
+    col_order: 7
+  },
+  {
+    id: 9,
+    label: 'third column',
+    col_order: 8
+  },
+  {
+    id: 10,
+    label: 'last column',
+    col_order: 9
   }
 ];
+const onDragEnd = (result, parsed_columns, setParsed_Columns) => {
+  console.log('i am dragging');
+  if (result.destination === null) {
+    return;
+  }
+  const { source, destination } = result;
+  if (source.droppableId !== destination.droppableId) {
+    const from_column = parsed_columns[source.droppableId];
+    const to_column = parsed_columns[destination.droppableId];
+    const column_array = [...parsed_columns];
+    const from_tasks = [...from_column.col_tasks];
+    const to_tasks = [...to_column.col_tasks];
+    const [removed] = from_tasks.splice(source.index, 1);
+    console.log('this is the removed task', removed);
+    console.log(to_column);
+    removed.col_id = to_column.id;
+    to_tasks.splice(destination.index, 0, removed);
+    to_tasks.forEach((task, index) => (task.index = index));
+    from_tasks.forEach((task, index) => (task.index = index));
+    column_array[source.droppableId].col_tasks = from_tasks;
+    column_array[destination.droppableId].col_tasks = to_tasks;
+    setParsed_Columns(column_array);
+    console.log('i tried to move between columns', from_tasks, to_tasks);
+  } else {
+    const column_array = [...parsed_columns];
+    const column = parsed_columns[source.droppableId];
+    const copied_tasks = [...column.col_tasks];
+    const [removed] = copied_tasks.splice(source.index, 1);
+    copied_tasks.splice(destination.index, 0, removed);
+    copied_tasks.forEach((task, index) => (task.index = index));
+    column_array[source.droppableId].col_tasks = copied_tasks;
+    setParsed_Columns(column_array);
+    console.log('after setting parsed columns', column_array);
+  }
+};
 export default function Tasks() {
   const [tasks, setTask] = useState(tasksfrombackend);
   const [columns, setColumns] = useState(columnsfrombackend);
+  //const [parsed_columns, setParsed_Columns] = useState([]);
+  const parsing_columns = [];
+  for (let i = 0; i < columns.length; i++) {
+    console.log(columns[i].label);
+    const parsed_object = {
+      id: columns[i].id,
+      label: columns[i].label,
+      col_order: columns[i].col_order,
+      col_tasks: tasks.filter((desired_tasks) => columns[i].id === desired_tasks.col_id)
+    };
+    parsing_columns.push(parsed_object);
+  }
+  const [parsed_columns, setParsed_Columns] = useState(parsing_columns);
+  //setParsed_Columns(parsing_columns);
+  console.log(parsed_columns);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
-      <DragDropContext onDragEnd={(result) => console.log(result)}>
-        {columns.map((column) => {
+    <div style={{ display: 'flex', justifyContent: 'left', height: '100%' }}>
+      <DragDropContext onDragEnd={(result) => onDragEnd(result, parsed_columns, setParsed_Columns)}>
+        {console.log('before the mapping', parsed_columns)}
+        {parsed_columns.map((column) => {
           return (
-            <Droppable droppableId={column.id.toString()}>
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                    style={{
-                      background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
-                      padding: 4,
-                      width: 250,
-                      minHeight: 500
-                    }}
-                  >
-                    {tasks
-                      .filter((desired_tasks) => column.id === desired_tasks.col_id)
-                      .map((task, index) => {
+            <div>
+              <h2>{column.label}</h2>
+              <Droppable droppableId={column.col_order.toString()} type="task" key={column.col_order}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      style={{
+                        background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                        padding: 4,
+                        width: 250,
+                        minHeight: 50
+                      }}
+                    >
+                      {column.col_tasks.map((task, index) => {
                         return (
                           <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
                             {(provided, snapshot) => {
@@ -96,8 +241,6 @@ export default function Tasks() {
                                   }}
                                 >
                                   {task.notes}
-                                  {console.log(index)}
-                                  {console.log('check')}
                                   {console.log(task.id)}
                                 </div>
                               );
@@ -105,11 +248,12 @@ export default function Tasks() {
                           </Draggable>
                         );
                       })}
-                    {provided.placeholder}
-                  </div>
-                );
-              }}
-            </Droppable>
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
           );
         })}
       </DragDropContext>
