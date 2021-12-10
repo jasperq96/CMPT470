30d6b22cb3ee8675fbd634be9ebcdd8fddc159e2
jasperq <jasperq@sfu.ca>
2021-12-10 15:36:19 -0800

    Added home page with css

--- dello-box-client/src/pages/Home.js
+++ dello-box-client/src/pages/Home.js
@@ -1,12 +1,24 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../hooks/UserContext';
+import '../stylesheets/home.css'
 
 export default function Home() {
   const userContext = useContext(UserContext);
 
   return (
     <div>
-      <h1>Welcome back {userContext.user?.username}</h1>
+      <p id='head1' class='header'>Welcome Back</p>
+      <p id='head2' class='header'>{userContext.user?.username}</p>
+      <p id='head3' class='header'>Dello-box of {userContext.user?.username}</p>
+      <div class='light x1'></div>
+      <div class='light x2'></div>
+      <div class='light x3'></div>
+      <div class='light x4'></div>
+      <div class='light x5'></div>
+      <div class='light x6'></div>
+      <div class='light x7'></div>
+      <div class='light x8'></div>
+      <div class='light x9'></div>
     </div>
   );
 }
