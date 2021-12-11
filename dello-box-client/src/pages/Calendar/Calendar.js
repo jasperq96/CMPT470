991817077ae13996000bccb1c3cd2dbc6a56feaa
jasperq <jasperq@sfu.ca>
2021-12-11 09:57:03 -0800

    36: Hooked up calendar to display title of users tasks

--- dello-box-client/src/pages/Calendar/Calendar.js
+++ dello-box-client/src/pages/Calendar/Calendar.js
@@ -1,34 +1,40 @@
 import '../../App.css';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect} from 'react';
+import httpService from '../../services/httpService';
+import { toast } from 'react-toastify';
 import Calendarlib from 'react-calendar';
 import { UserContext } from '../../hooks/UserContext';
 import DatePicker from 'react-datepicker';
 import { Container, Row, Col } from 'react-bootstrap';
+import { capitalize } from '../../utils/capitalizeString';
 import 'react-calendar/dist/Calendar.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const Calendar = () => {
   const userContext = useContext(UserContext);
   const [date, setDate] = useState(new Date());
+  const [task, setTask] = useState([]);
+
   const onDate = (newDate) => {
     if (newDate !== null) {
       setDate(newDate);
     }
   };
-  const temp_dates = [
-    {
-      date: '2021-11-14T10:30:00.000Z', //this is nov 14
-      info: 'this is what your doing on nov 11'
-    },
-    {
-      date: '2021-11-13T10:30:00.000Z',
-      info: 'this is to check if i can put multiple things on one list' //this is
-    },
-    {
-      date: '2021-11-13T10:30:00.000Z',
-      info: 'this is to check if i can test put multiple things on one list' //this is
+
+  const getTasks = async () => {
+    const url = `/task/${userContext.user?.id}`;
+    try {
+      const response = await httpService.get(url);
+      setTask(response.data);
+    } catch (error) {
+      toast.error('Error: '.concat(capitalize(error.response.data.error)));
     }
-  ];
+  };
+  
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
     <div>
       <Container fluid>
@@ -38,10 +44,11 @@ const Calendar = () => {
             <DatePicker selected={date} onChange={(date) => setDate(date)} readOnly />
           </Col>
           <Col lg>
-            {temp_dates
-              .filter((stored_date) => stored_date.date.substring(0, 10) === date.toISOString().substring(0, 10))
+            {task
+              .filter((stored_date) => stored_date.start_date.substring(0, 10) <= date.toISOString().substring(0, 10) &&
+              stored_date.end_date.substring(0, 10) >= date.toISOString().substring(0, 10))
               .map((filtered_Dates) => (
-                <li className="body-color">{filtered_Dates.info}</li>
+                <li className="body-color">{filtered_Dates.title}</li>
               ))}
             {console.log(date.toISOString)}
           </Col>
