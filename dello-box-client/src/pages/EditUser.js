c64b008c321d524f3aebcce5348c48eed3aac874
ygyang <ygyang@sfu.ca>
2021-12-11 21:08:40 -0800

    finished page for editing user info

--- dello-box-client/src/pages/EditUser.js
+++ dello-box-client/src/pages/EditUser.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button, Container } from 'react-bootstrap';
 import { toast } from 'react-toastify';
+
+const dummy_data = {
+  user_id: 1,
+  first_name: 'Jane',
+  last_name: 'Doe',
+  email: 'janedoe@gmail.com',
+  phone: '604-123-4567'
+};
+
 export default function EditUser() {
-  const [data, setData] = useState();
   const [values, setValue] = useState({
-    title: '',
-    notes: '',
-    start_date: '',
-    start_time: '',
-    end_date: '',
-    end_time: '',
-    col_id: ''
-  });
-  const [colValue, setColValue] = useState({
-    title: ''
+    user_id: 1,
+    first_name: dummy_data.first_name,
+    last_name: dummy_data.last_name,
+    email: dummy_data.email,
+    phone: dummy_data.phone
   });
   const handleChange = (evt) => {
     setValue({
@@ -26,62 +29,35 @@ export default function EditUser() {
   };
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    const parsing_Object = { ...values };
-    console.log(parsing_Object);
-    const comb_start = parsing_Object.start_date.concat(' ', parsing_Object.start_time);
-    const comb_end = parsing_Object.end_date.concat(' ', parsing_Object.end_time);
-    if (parsing_Object.start_date === '') {
-      toast.error('You didnt set a Start Date');
-      return;
-    }
-    if (parsing_Object.end_date === '') {
-      toast.error('You didnt set a End Date');
-      return;
-    }
-    const newStartDate = new Date(comb_start).toISOString();
-    const newEndDate = new Date(comb_end).toISOString();
-    const forBackend = {
-      colId: parsing_Object.col_id,
-      startDate: newStartDate,
-      endDate: newEndDate,
-      title: parsing_Object.title,
-      notes: parsing_Object.notes
-    };
-    console.log('This is for', forBackend);
+    alert(`these are the entered values ${values.first_name}
+    ${values.last_name}
+    ${values.email}
+    ${values.phone}`);
   };
 
   return (
     <Container fluid>
       <h1 className="WhiteHeaders">Creating a Task</h1>
       <Form>
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label className="WhiteHeaders">Task Title</Form.Label>
-          <Form.Control type="Text" placeholder="Task Title" name="title" onChange={handleChange} />
-        </Form.Group>
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label className="WhiteHeaders">Task Notes</Form.Label>
-          <Form.Control type="Text" placeholder="Task Notes" as="textarea" rows={3} name="notes" onChange={handleChange} />
-        </Form.Group>
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label className="WhiteHeaders">Start Date</Form.Label>
-            <Form.Control type="Date" placeholder="First Day of The Task" name="start_date" onChange={handleChange} />
+            <Form.Label className="WhiteHeaders">First Name</Form.Label>
+            <Form.Control type="Name" placeholder="First Day of The Task" name="first_name" value={values.first_name} onChange={handleChange} />
           </Form.Group>
           <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label className="WhiteHeaders">Time of Event</Form.Label>
-            <Form.Control type="Time" placeholder="Time of Event" name="start_time" onChange={handleChange} />
+            <Form.Label className="WhiteHeaders">Last Name</Form.Label>
+            <Form.Control type="Name" placeholder="Time of Event" name="last_name" value={values.last_name} onChange={handleChange} />
           </Form.Group>
         </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label className="WhiteHeaders">End Date</Form.Label>
-            <Form.Control type="Date" placeholder="First Day of The Task" name="end_date" onChange={handleChange} />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label className="WhiteHeaders">Time of Event End</Form.Label>
-            <Form.Control type="Time" placeholder="Time of Event" name="end_time" onChange={handleChange} />
-          </Form.Group>
-        </Row>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label className="WhiteHeaders">Email</Form.Label>
+          <Form.Control type="Email" placeholder="Task Title" name="email" value={values.email} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label className="WhiteHeaders">Phone</Form.Label>
+          <Form.Control type="PhoneNumber" placeholder="Task Notes" name="phone" value={values.phone} onChange={handleChange} />
+        </Form.Group>
+        <Button onClick={(e) => handleSubmit(e)}>Change user Info</Button>
       </Form>
     </Container>
   );
